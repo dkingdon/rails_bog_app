48e5e6d1f1536212fc45fb97cb1ca036f86674cd
dkingdon <daniel.kingdon@gmail.com>
2016-12-18 15:21:20 -0800

    Part 4 complete. now time to do it again. only faster!

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -9,7 +9,6 @@ class CreaturesController < ApplicationController
   end
 
   def create
-    creature_params = params.require(:creature).permit(:name, :description)
     creature = Creature.new(creature_params)
     if creature.Save
       redirect_to creatures_path(creature)
@@ -23,6 +22,25 @@ class CreaturesController < ApplicationController
   def edit
     creature_id = params[:id]
     @creature = Creature.find_by_id(creature_id)
+  end
+
+  def update
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+    redirect_to creature_path(creature)
+  end
+
+  def destroy
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+    creature.destroy
+    redirect_to creature_path
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
   end
 
 end
