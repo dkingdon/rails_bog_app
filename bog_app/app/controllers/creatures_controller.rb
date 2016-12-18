764d32026de287575203da673a33b91b7a45d65e
dkingdon <daniel.kingdon@gmail.com>
2016-12-17 22:19:47 -0800

    done!!! now time to do it again only faster! FASTER FAAAAASTER!!!

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -14,13 +14,36 @@ class CreaturesController < ApplicationController
   end
 
   def create
-    creature_params = params.require(:creature).permit(:name, :description)
-
     creature = Creature.new(creature_params)
 
     if creature.save
       redirect_to creature_path(creature)
     end
   end
+
+  def edit
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+  end
+
+  def update
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+  end
+
+  def destroy
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+    creature.destroy
+    redirect_to creatures_path
+  end
+
+  private
+
+    def creature_params
+      params.require(:creature).permit(:name, :description)
+    end
 
 end
