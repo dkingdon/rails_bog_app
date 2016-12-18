979d2a5be5e98c57d1acf6415d758f53a99a6257
dkingdon <daniel.kingdon@gmail.com>
2016-12-18 13:13:14 -0800

    part two complete

--- bog_app/bog_app/config/routes.rb
+++ bog_app/bog_app/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
-  get '/creatures', to: 'creatures#index', as: 'creatures' 
-
+  get '/creatures', to: 'creatures#index', as: 'creatures'
+  get '/creatures/new', to: 'creatures#new', as: 'new_creature'
+  post '/creatures', to: 'creatures#create'
+  get '/creature/:id', to: 'creature#show', as: 'creature'
 end
