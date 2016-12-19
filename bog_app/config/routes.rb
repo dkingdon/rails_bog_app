c1a493596541b4819b193ce1b933f693c1a27bd3
dkingdon <daniel.kingdon@gmail.com>
2016-12-18 23:12:56 -0800

    fourth part complete. Destroy

--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   get '/creatures/:id', to: 'creatures#show', as: 'creature'
   get '/creatures/:id/edit', to: 'creatures#edit', as: 'edit_creature'
   patch '/creatures/:id', to: 'creatures#update'
+  delete '/creature/:id', to: 'creature#destroy'
 end
